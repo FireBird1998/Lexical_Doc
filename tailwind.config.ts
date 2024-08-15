f5664ffa7f10bccb4ed4cbf75541bca4b4644415
ankit-thesys <i-ankit@thesys.dev>
2024-08-15 23:20:39 +0530

    more setup and adding lexical editor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 const config = {
   darkMode: ['class'],
@@ -64,6 +64,6 @@ const config = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+} satisfies Config
 
-export default config;
+export default config
