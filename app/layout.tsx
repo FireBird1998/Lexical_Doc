9209a418e1fc54832f75257a29319f4194ee2a3e
ankit-thesys <i-ankit@thesys.dev>
2024-08-15 20:43:23 +0530

    more setup process

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,29 @@ import '@/styles/globals.css'
 import { Inter as FontSans } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
+import { Metadata } from 'next'
 
 const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 })
 
+export const metadata: Metadata = {
+  title: 'My Docs',
+  description: 'My app description',
+}
+
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
+        className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
       >
-        ...
+        {children}
       </body>
     </html>
   )
