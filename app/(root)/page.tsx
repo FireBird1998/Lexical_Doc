ab3923306b4c4ad8d41152c83b5a8d007b218375
ankit-thesys <i-ankit@thesys.dev>
2024-08-16 04:30:24 +0530

    fixing configuration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 
 export default function Home() {
-	return (
-		<>
-			<Button>Click me </Button>
-		</>
-	)
+	return <Button>Click me </Button>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ interface RootLayoutProps {
 	children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body
